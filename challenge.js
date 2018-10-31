4d18904f9cfb77f251354d19cead4a23f68d9cde
Cristina Murillo <murillocam96@gmail.com>
2018-10-31 17:06:40 -0400

    we are geniuses

---
+++ challenge.js
@@ -0,0 +1,46 @@
+const timerId = setInterval(incrementTime, 1000);
+
+let timerThing = parseInt(document.querySelector('#counter').innerText)
+let timerLocation = document.querySelector('#counter')
+
+function incrementTime(){
+
+ timerLocation.innerText = timerThing + 1;
+ timerThing += 1
+}
+
+
+const minus = document.getElementById('-')
+const plus = document.getElementById('+')
+const heart = document.getElementById('<3')
+let currentTime = null;
+let timeHash = {}
+
+minus.addEventListener('click', decrementTime)
+plus.addEventListener('click', incrementTime)
+heart.addEventListener('click', () => {
+if (timeHash[timerThing]){
+    let oldCount = timeHash[timerThing]
+    timeHash[timerThing] = oldCount + 1
+    document.getElementById(timerThing).innerText = `${timerThing} has been liked ${timeHash[timerThing]} times`
+ } else {
+  addList()
+ } 
+})
+
+function decrementTime(){
+    
+    timerLocation.innerText = timerThing - 1;
+    timerThing -= 1
+}
+
+const list = document.createElement('ul');
+document.body.appendChild(list);
+
+function addList() {
+    let li = document.createElement('li');
+    li.id = timerThing 
+    timeHash[timerThing] = 1
+    li.innerHTML = `${timerThing} has been liked 1 time`
+    list.appendChild(li)
+}  
